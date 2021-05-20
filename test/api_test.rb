bc096e3dde51d244939d97d98082c491837ca9c6
Nick Edwin <nickedwin85@gmail.com>
2021-05-20 10:59:49 -0600

    Formatted test to do Faraday call before running any one test

--- test/api_test.rb
+++ test/api_test.rb
@@ -3,12 +3,13 @@ require File.expand_path('../support/test_helper', __dir__)
 require 'minitest/autorun'
 
 class ApiTest < Minitest::Test
-
-  def test_no_api_key
+  def before_setup
     def conn
       Faraday.new(url: "http://www.omdbapi.com/")
     end
+  end
 
+  def test_no_api_key
     response = conn.get("?t=movies")
     json = JSON.parse(response.body, symbolize_names: true)
 
@@ -17,6 +18,4 @@ class ApiTest < Minitest::Test
     ## Test shows that when API Key not provided user can not retrieve data from API
     ## Reponse == {:Response=>"False", :Error=>"No API key provided."}
   end
-
-  
 end
